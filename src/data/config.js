1ae06cb17632eb6b28f718e512bfab1b7a5b7cb6
xionghuiYu <3084554188@qq.com>
2026-04-18 15:38:08 +0800

    chore: 创建工具配置

---
+++ src/data/config.js
@@ -0,0 +1,26 @@
+export const tools = [
+  {
+    id: 'todo',
+    name: '待办清单',
+    description: '管理你的任务和待办事项',
+    icon: 'check-square',
+    route: '/tools/todo',
+    color: 'bg-blue-500'
+  },
+  {
+    id: 'pomodoro',
+    name: '番茄钟',
+    description: '专注工作 25 分钟，休息 5 分钟',
+    icon: 'timer',
+    route: '/tools/pomodoro',
+    color: 'bg-red-500'
+  },
+  {
+    id: 'notes',
+    name: '快速笔记',
+    description: '快速记录想法和灵感',
+    icon: 'file-text',
+    route: '/tools/notes',
+    color: 'bg-green-500'
+  }
+]
