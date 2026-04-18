2a5657e89807149c6f0f0c422694e43357fda1a9
xionghuiYu <3084554188@qq.com>
2026-04-18 15:40:17 +0800

    chore: 添加工具路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import Home from '../pages/Home.vue'
 import Blog from '../pages/Blog.vue'
 import BlogDetail from '../pages/BlogDetail.vue'
 import Bookmarks from '../pages/Bookmarks.vue'
+import Tools from '../pages/Tools.vue'
+import TodoTool from '../pages/TodoTool.vue'
+import PomodoroTool from '../pages/PomodoroTool.vue'
+import NotesTool from '../pages/NotesTool.vue'
 import NotFound from '../pages/NotFound.vue'
 
 const routes = [
@@ -25,6 +29,26 @@ const routes = [
     path: '/bookmarks',
     name: 'bookmarks',
     component: Bookmarks
+  },
+  {
+    path: '/tools',
+    name: 'tools',
+    component: Tools
+  },
+  {
+    path: '/tools/todo',
+    name: 'todo-tool',
+    component: TodoTool
+  },
+  {
+    path: '/tools/pomodoro',
+    name: 'pomodoro-tool',
+    component: PomodoroTool
+  },
+  {
+    path: '/tools/notes',
+    name: 'notes-tool',
+    component: NotesTool
   },
   {
     path: '/:pathMatch(.*)*',
