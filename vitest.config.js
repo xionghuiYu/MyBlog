8235ad3f9c6815a6ffdd147d3e14738d42498473
xionghuiYu <3084554188@qq.com>
2026-04-18 15:42:39 +0800

    chore: 配置 Vitest

---
+++ vitest.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'tests/',
+        '**/*.test.js',
+        '**/*.spec.js',
+        '**/dist/'
+      ]
+    }
+  }
+})
