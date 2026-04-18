671e73d10054f21c042542e34eed9c166f9747f0
xionghuiYu <3084554188@qq.com>
2026-04-18 15:39:55 +0800

    feat: 创建番茄钟页面

---
+++ src/pages/PomodoroTool.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen py-12">
+    <PomodoroTimer />
+  </div>
+</template>
+
+<script setup>
+import PomodoroTimer from '../components/tools/PomodoroTimer.vue'
+</script>
