965ea4e9cdc98acaec45e155f87fd5e25ccf6e54
xionghuiYu <3084554188@qq.com>
2026-04-18 15:39:39 +0800

    feat: 创建工具列表页面

---
+++ src/pages/Tools.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen">
+    <section class="py-12">
+      <ToolList />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import ToolList from '../components/tools/ToolList.vue'
+</script>
